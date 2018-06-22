0faf8c6a23b8bba328d008a9cc2235262d99273b
Nick <nickgrajecki@gmail.com>
2018-06-22 14:05:29 +0100

    Added backend
    Added a lot of backend functionality

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ function readConfig(configJSON) {
     // var config = require('./read');
     // console.log(configJSON);
     var config = configJSON;
-    
+
     // var config = {
     //     "firstaid": true,
     //     "emergency": true,
@@ -316,4 +316,26 @@ function pickModules() {
         localStorage.setItem("emergencycheck", false);
     }
     alert('Form submitted!');
+}
+
+function firstAid() {
+    var name;
+    var age;
+    var height;
+    $.getJSON('/details.json', function (json) {
+        if (json !== null){
+        name = json.name;
+        age = json.age;
+        height = json.height;
+        $("#username").replaceWith("<h2>" + name + "</h2>");
+        $("#age").replaceWith("<h2>" + age + "</h2>");
+        $("#userheight").replaceWith("<h2>" + height + "</h2>");
+        $("#toptext").replaceWith("<h2>These are your details:</h2>");
+        $("#submit").remove();
+        console.log(name + age + height);
+        } 
+    });
+
+
+
 }
--- server.js
+++ server.js
@@ -1,25 +1,105 @@
+//Get all the necessary modules
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var file = './config.json'
 
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static(__dirname));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-  var data = JSON.parse(decodeURI(fs.readFileSync('config.json', 'utf8')));
-  res.render('home', {data: data});
+//Home page
+app.get('/', function (req, res) {
+  res.render('home');
 });
 
-app.get('/:name', function(req, res){
-  var data = {age: 29, job: 'ninja', hobby: ['eating', 'fighting', 'fooding']};
-  res.render('module', {name: req.params.name, data: data});
+//Module selection under settings redirect
+app.get('/modules', function (req, res) {
+  res.render('modules');
 });
 
-app.post('/feedback', urlencodedParser, function(req, res){
-  res.render('module', {name: req.params.name, data: data});
+//Process module selection
+app.post('/modules', urlencodedParser, function (req, res) {
+  var firstaid = req.body.firstaidc;
+  if (firstaid === "on") {
+    firstaid = true;
+  } else {
+    firstaid = false;
+  }
+  var video = req.body.videoc;
+  if (video === "on") {
+    video = true;
+  } else {
+    video = false;
+  }
+
+  var presc = req.body.prescc;
+  if (presc === "on") {
+    presc = true;
+  } else {
+    presc = false;
+  }
+
+  var hospital = req.body.hospitalc;
+  if (hospital === "on") {
+    hospital = true;
+  } else {
+    hospital = false;
+  }
+
+  var diary = req.body.diaryc;
+  if (diary === "on") {
+    diary = true;
+  } else {
+    diary = false;
+  }
+  var nutrition = req.body.nutritionc;
+  if (nutrition === "on") {
+    nutrition = true;
+  } else {
+    nutrition = false;
+  }
+  var symptom = req.body.symptomc;
+  if (symptom === "on") {
+    symptom = true;
+  } else {
+    symptom = false;
+  }
+  var emergency = req.body.emergencyc;
+  if (emergency === "on") {
+    emergency = true;
+  } else {
+    emergency = false;
+  }
+  var obj = { firstaid: firstaid, video: video, presc: presc, hospital: hospital, diary: diary, nutrition: nutrition, symptom: symptom, emergency: emergency };
+  console.log(obj);
+  fs.writeFileSync(file, JSON.stringify(obj));
+  res.render('modules');
 })
 
-app.listen(3000);
+//First aid module
+app.get('/firstaid', function (req, res) {
+  fs.readFile(__dirname + '/sp_modules/firstaid.html', 'utf8', function (err, html) {
+    if (err) {
+      throw err
+    } else {
+      // console.log(html)
+      res.render('module', { content: html });
+    }
+  });
+});
 
+//Process firstaid module
+app.post('/firstaid', urlencodedParser, function (req, res) {
+  var detailsFile = './details.json'
+  var name = req.body.username;
+  var age = req.body.age;
+  var height = req.body.userheight;
+  var obj = { name: name, age: age, height: height };
+  console.log(obj)
+  fs.writeFileSync(detailsFile, JSON.stringify(obj));
+  res.redirect('/firstaid');
+});
+
+app.listen(3000);
