9096f7cee563a003c6a4d445488d28363e70d5d0
Nick <nickgrajecki@gmail.com>
2018-06-23 17:37:40 +0100

    Implemented MongoDB
    Implement MongoDB, added a login screen for testing.

--- server.js
+++ server.js
@@ -2,7 +2,34 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var file = './config.json'
+
+//Connect to database
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://admin:insertpass@ds161700.mlab.com:61700/simpleplus');
+var userSchema = new mongoose.Schema({
+  name: String,
+  age: String,
+  height: String,
+  // modules: {
+  //   firstaid: String,
+  //   video: String,
+  //   presc: String,
+  //   hospital: String,
+  //   diary: String,
+  //   nutrition: String,
+  //   symptom: String,
+  //   emergency: String
+  // }
+});
+
+var file = './config.json';
+var detailsFile = './details.json';
+var parseDetails = JSON.parse(fs.readFileSync(detailsFile, 'utf8'));
+var userName = parseDetails.name;
+var userAge = parseDetails.age;
+var userHeight = parseDetails.height;
+
+var dbUser = mongoose.model('User', userSchema);
 
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -11,12 +38,30 @@ app.set('view engine', 'ejs');
 
 //Home page
 app.get('/', function (req, res) {
-  res.render('home');
+  res.render('login');
+});
+
+app.post('/', urlencodedParser, function (req, res) {
+  var name = req.body.username;
+  var sendname = dbUser.find({ username: name }, { _id: 0, username: 1 });
+  sendname.exec(function (err, data) {
+    console.log(data);
+    if (err) {
+      throw err;
+    } else if (data.length) {
+      data = JSON.parse(JSON.stringify(data));
+      console.log(data[0].username);
+      res.render('home', { name: data[0].username });
+    } else {
+      //Add code for what happens when user doesn't exist
+      res.redirect('/', { message: 'User does not exist' });
+    }
+  })
 });
 
 //Module selection under settings redirect
 app.get('/modules', function (req, res) {
-  res.render('modules');
+  res.render('modules', { name: userName });
 });
 
 //Process module selection
@@ -92,10 +137,20 @@ app.get('/firstaid', function (req, res) {
 
 //Process firstaid module
 app.post('/firstaid', urlencodedParser, function (req, res) {
-  var detailsFile = './details.json'
   var name = req.body.username;
   var age = req.body.age;
   var height = req.body.userheight;
+  userSchema.find({ name: name }, function (error, comments) {
+    console.log(comments);
+  })
+  var mongouser = dbUser({
+    name: req.body.username,
+    age: req.body.age,
+    height: req.body.userheight
+  }).save(function (err) {
+    if (err) throw err;
+    console.log('user saved')
+  });
   var obj = { name: name, age: age, height: height };
   console.log(obj)
   fs.writeFileSync(detailsFile, JSON.stringify(obj));
